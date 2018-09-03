ec7ba37539d14cabeca77707d96480b796bed023
jcmedinanix <jcmedinanix@gmail.com>
2018-09-03 12:37:39 -0500

    Add files via upload

---
+++ datos/configuracion.php
@@ -0,0 +1,17 @@
+<?php
+
+// Notificar todos los errores excepto E_NOTICE
+error_reporting(0);
+/*
+define("SERVIDOR_BD", "ec2-107-22-221-60.compute-1.amazonaws.com");
+define("USUARIO_BD", "qbtlvmhscxojvb");
+define("CLAVE_BD", "8ce0dd30dac4119d90d27f0eef6cc93735e60f2b90c62ccaac3f4a032ad74b9e");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "d8in90ck27ji1a");
+*/
+
+define("SERVIDOR_BD", "ec2-174-129-22-84.compute-1.amazonaws.com");
+define("USUARIO_BD", "ibcoblediyvudj");
+define("CLAVE_BD", "94e2248952b9f9ff910151219b34d058faaeacb08a3c6da77ae8ca62d6f7adce");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "dbg8shkqtus9hp");
